ff78f95d1aa87238c10e55ce57af0b644e208316
Mazin Fouad <m.fouad@live.de>
2023-02-26 13:47:40 +0100

    logic for get data from the inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -40,6 +42,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatFormFieldModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./dialog-add-user.component.scss'],
 })
 export class DialogAddUserComponent {
+  user: User = new User();
+  birthDate: Date;
+
+  constructor() {
+    this.user.firstName;
+    this.user.lastName;
+    this.user.birthDate;
+    this.user.street;
+    this.user.zipCode;
+    this.user.city;
+  }
+
   onNoClick() {}
+
+  saveUserData() {
+    console.log('User Data', this.user);
+    this.user.birthDate = this.birthDate.getTime();
+  }
 }
