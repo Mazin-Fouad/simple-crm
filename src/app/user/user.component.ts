8fdfd9ad3702474a4f1f08913dca42bd955e72a7
Mazin Fouad <m.fouad@live.de>
2023-02-26 11:15:27 +0100

    inputs mask for create user added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent {}
+export class UserComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogAddUserComponent, {});
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
+}
